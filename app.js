854e7a875deedcfb700839c4b40e7b9236145095
Rohith <gnsprathyu06@gmail.com>
2023-04-28 12:52:15 +0530

    rohith added again

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-((function)
-{
-  'use strict';
-
-  angular.module('myFirstApp',[])
-  .controller('MyFirstController',function ($scope) {
-$scope.name="Rohith";
-$scope.sayhello=function () {
-  return "Hello Mike";
-} () {
-
-}
-  });
-})();
+function(
+  {
+    
+  }
+)
